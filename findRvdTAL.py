a7b921e444d66c498877f441a0341df626316f08
Nick Booher <njbooher@gmail.com>
2012-06-03 20:09:03 -0500

    Up the number of processors used to 2

--- findRvdTAL.py
+++ findRvdTAL.py
@@ -52,7 +52,7 @@ def RunTalesfTask(options):
 	else:
 		seqFilename = options.fasta
 	
-	result = ScoreTalesfTask(seqFilename, rvdString, options.outputFilepath, options.logFilepath, forwardOnly, options.cupstream, options.cutoff, 1, options.organism if options.genome else "")
+	result = ScoreTalesfTask(seqFilename, rvdString, options.outputFilepath, options.logFilepath, forwardOnly, options.cupstream, options.cutoff, 2, options.organism if options.genome else "")
 	
 	if(result == 1):
 		raise TaskError()
