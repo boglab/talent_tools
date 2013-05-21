c83491d14964002138df9484240ab32d0fd5cc7b
Nick Booher <njbooher@gmail.com>
2013-05-21 14:34:05 -0400

    Download Entrez to a tempfile if Redis isn't available

--- entrez_cache.py
+++ entrez_cache.py
@@ -1,213 +1,266 @@
-import redis
 import pickle
 import uuid
 import time
 import os
+import tempfile
 from Bio import Entrez, SeqIO
 from talconfig import BASE_DIR, REDIS_SERVER_HOSTNAME, REDIS_SERVER_PORT
 from talutil import TaskError
 
 
+redis_found = True
+try:
+    import redis
+except ImportError:
+    redis_found = False
+
 # redis locking
 # http://www.dr-josiah.com/2012/01/creating-lock-with-redis.html
 
-class CachedEntrezFile(object):
+# TODO
+# Add non-caching implementation if celery isn't found
+
+if redis_found:
     
-    WRITE_LOCK_DURATION = 3600
-    READ_LOCK_DURATION = 36000
-    
-    CACHED_FILE_DIR = BASE_DIR + "/cached_gb_files"
-    CACHED_FILE_PATTERN = CACHED_FILE_DIR + "/%s.fasta"
-    
-    def __init__(self, seq_id):
+    class CachedEntrezFile(object):
         
-        self.conn = redis.StrictRedis(host=REDIS_SERVER_HOSTNAME, port=REDIS_SERVER_PORT, db=1)
+        WRITE_LOCK_DURATION = 3600
+        READ_LOCK_DURATION = 36000
         
-        try:
-            Entrez.email = "6e6a62393840636f726e656c6c2e656475".decode("hex")
-            Entrez.tool = "https://tale-nt.cac.cornell.edu"
-            search_handle = Entrez.esearch(db="nucleotide", term=seq_id)
-            search_record = Entrez.read(search_handle)
-            search_handle.close()
-            self.seq_id = search_record["IdList"][0]
-        except IOError:
-            raise TaskError("Invalid sequence ID provided")
+        CACHED_FILE_DIR = BASE_DIR + "/cached_gb_files"
+        CACHED_FILE_PATTERN = CACHED_FILE_DIR + "/%s.fasta"
         
-        self.lock_name = "gb_cache:%s" % self.seq_id
-        self.uuid = str(uuid.uuid4())
-        self.filepath = CachedEntrezFile.CACHED_FILE_PATTERN % self.seq_id
-        self.file = None
-    
-    def __enter__(self):
-        
-        pipe = self.conn.pipeline(True)
-        
-        while True:
+        def __init__(self, seq_id):
+            
+            self.conn = redis.StrictRedis(host=REDIS_SERVER_HOSTNAME, port=REDIS_SERVER_PORT, db=1)
             
             try:
+                Entrez.email = "6e6a62393840636f726e656c6c2e656475".decode("hex")
+                Entrez.tool = "https://tale-nt.cac.cornell.edu"
+                search_handle = Entrez.esearch(db="nucleotide", term=seq_id)
+                search_record = Entrez.read(search_handle)
+                search_handle.close()
+                self.seq_id = search_record["IdList"][0]
+            except IOError:
+                raise TaskError("Invalid sequence ID provided")
+            
+            self.lock_name = "gb_cache:%s" % self.seq_id
+            self.uuid = str(uuid.uuid4())
+            self.filepath = CachedEntrezFile.CACHED_FILE_PATTERN % self.seq_id
+            self.file = None
+        
+        def __enter__(self):
+            
+            pipe = self.conn.pipeline(True)
+            
+            while True:
                 
-                pipe.watch(self.lock_name)
-                
-                if not pipe.exists(self.lock_name):
-                    
-                    print("No entry for key %s, creating" % self.lock_name)
-                    
-                    timestamp = time.time()
-                    
-                    pipe.multi()
-                    
-                    pipe.hmset(self.lock_name, {
-                        "last-accessed": timestamp,
-                        "readers": pickle.dumps({}),
-                        "writers": pickle.dumps({
-                            self.uuid: timestamp + CachedEntrezFile.WRITE_LOCK_DURATION
-                        })
-                    })
-                    
-                    pipe.execute()
-                    
-                    print("Key %s created" % self.lock_name)
-                    print("Downloading file for newly created key %s" % self.lock_name)
-                    
-                    try:
-                        os.remove(self.filepath)
-                    except OSError:
-                        pass
-                    
-                    try:
-                        
-                        fetch_handle = Entrez.efetch(db="nucleotide", id=self.seq_id, rettype="fasta", retmode="text")
-                        fetch_record = SeqIO.read(fetch_handle, "fasta")
-                        fetch_handle.close()
-                        
-                        with open(self.filepath, "w") as cached_file:
-                            SeqIO.write([fetch_record], cached_file, "fasta")
-                        
-                    except IOError:
-                        # I dunno
-                        pass
-                    
-                    #subprocess.check_call("rm -f %s/chromFa.tar.gz" % CachedEntrezFile.CACHED_FILE_DIR, shell=True)
-                    #subprocess.check_call("wget -O %s/chromFa.tar.gz ftp://hgdownload.cse.ucsc.edu/goldenPath/currentGenomes/Homo_sapiens/bigZips/chromFa.tar.gz" % CachedEntrezFile.CACHED_FILE_DIR, shell = True)
-                    
-                    print("File for key %s finished downloading, removing writer")
+                try:
                     
                     pipe.watch(self.lock_name)
                     
-                    lock_writers = pickle.loads(pipe.hget(self.lock_name, "writers"))
-                    
-                    if self.uuid in lock_writers:
-                        del lock_writers[self.uuid]
-                    
-                    pipe.multi()
-                    pipe.hset(self.lock_name, "writers", pickle.dumps(lock_writers))
-                    pipe.execute()
-                    
-                    print("Writer for key %s removed" % self.lock_name)
-                    
-                else:
-                    
-                    print("Key %s exists, checking for writer" % self.lock_name)
-                    
-                    timestamp = time.time()
-                    
-                    lock_writers = pickle.loads(pipe.hget(self.lock_name, "writers"))
-                    
-                    if len(lock_writers.keys()) > 0:
+                    if not pipe.exists(self.lock_name):
                         
-                        print("Writer for key %s exists, checking expiration" % self.lock_name)
-                        
-                        writers_changed = False
-                        
-                        for l_uuid, l_timestamp in lock_writers.copy().items():
-                            
-                            print("Writer for key %s with UUID %s, timestamp %s, current time %s" % (self.lock_name, l_uuid, str(l_timestamp), str(timestamp)))
-                            
-                            if l_uuid == self.uuid:
-                                print("Writer for key %s has our UUID, previous attempt to remove writer failed, retrying" % self.lock_name)
-                                del lock_writers[l_uuid]
-                                writers_changed = True
-                            
-                            if l_timestamp < timestamp:
-                                print("Writer for %s with UUID %s expired, removing" % (self.lock_name, l_uuid))
-                                del lock_writers[l_uuid]
-                                writers_changed = True
-                        
-                        if writers_changed:
-                            
-                            pipe.multi()
-                            #pipe.hset(self.lock_name, "writers", pickle.dumps(lock_writers))
-                            # since file will only be downloaded if key doesn't exist (because there's no way to check if local sequence
-                            # file is corrupted), and there's only one writer at a time, remove key so that redownload of file is attempted
-                            pipe.delete(self.lock_name)
-                            pipe.execute()
-                            
-                            print("Expired writers for key %s removed" % self.lock_name)
-                        
-                        time.sleep(5)
-                    
-                    else:
-                        
-                        print("No writers for key %s, adding reader" % self.lock_name)
+                        print("No entry for key %s, creating" % self.lock_name)
                         
                         timestamp = time.time()
                         
-                        lock_readers = pickle.loads(pipe.hget(self.lock_name, "readers"))
+                        pipe.multi()
                         
-                        lock_readers[self.uuid] = timestamp
+                        pipe.hmset(self.lock_name, {
+                            "last-accessed": timestamp,
+                            "readers": pickle.dumps({}),
+                            "writers": pickle.dumps({
+                                self.uuid: timestamp + CachedEntrezFile.WRITE_LOCK_DURATION
+                            })
+                        })
+                        
+                        pipe.execute()
+                        
+                        print("Key %s created" % self.lock_name)
+                        print("Downloading file for newly created key %s" % self.lock_name)
+                        
+                        try:
+                            os.remove(self.filepath)
+                        except OSError:
+                            pass
+                        
+                        try:
+                            
+                            fetch_handle = Entrez.efetch(db="nucleotide", id=self.seq_id, rettype="fasta", retmode="text")
+                            fetch_record = SeqIO.read(fetch_handle, "fasta")
+                            fetch_handle.close()
+                            
+                            with open(self.filepath, "w") as cached_file:
+                                SeqIO.write([fetch_record], cached_file, "fasta")
+                            
+                        except IOError:
+                            # I dunno
+                            pass
+                        
+                        #subprocess.check_call("rm -f %s/chromFa.tar.gz" % CachedEntrezFile.CACHED_FILE_DIR, shell=True)
+                        #subprocess.check_call("wget -O %s/chromFa.tar.gz ftp://hgdownload.cse.ucsc.edu/goldenPath/currentGenomes/Homo_sapiens/bigZips/chromFa.tar.gz" % CachedEntrezFile.CACHED_FILE_DIR, shell = True)
+                        
+                        print("File for key %s finished downloading, removing writer")
+                        
+                        pipe.watch(self.lock_name)
+                        
+                        lock_writers = pickle.loads(pipe.hget(self.lock_name, "writers"))
+                        
+                        if self.uuid in lock_writers:
+                            del lock_writers[self.uuid]
                         
                         pipe.multi()
-                        pipe.hset(self.lock_name, "last-accessed", timestamp)
+                        pipe.hset(self.lock_name, "writers", pickle.dumps(lock_writers))
+                        pipe.execute()
+                        
+                        print("Writer for key %s removed" % self.lock_name)
+                        
+                    else:
+                        
+                        print("Key %s exists, checking for writer" % self.lock_name)
+                        
+                        timestamp = time.time()
+                        
+                        lock_writers = pickle.loads(pipe.hget(self.lock_name, "writers"))
+                        
+                        if len(lock_writers.keys()) > 0:
+                            
+                            print("Writer for key %s exists, checking expiration" % self.lock_name)
+                            
+                            writers_changed = False
+                            
+                            for l_uuid, l_timestamp in lock_writers.copy().items():
+                                
+                                print("Writer for key %s with UUID %s, timestamp %s, current time %s" % (self.lock_name, l_uuid, str(l_timestamp), str(timestamp)))
+                                
+                                if l_uuid == self.uuid:
+                                    print("Writer for key %s has our UUID, previous attempt to remove writer failed, retrying" % self.lock_name)
+                                    del lock_writers[l_uuid]
+                                    writers_changed = True
+                                
+                                if l_timestamp < timestamp:
+                                    print("Writer for %s with UUID %s expired, removing" % (self.lock_name, l_uuid))
+                                    del lock_writers[l_uuid]
+                                    writers_changed = True
+                            
+                            if writers_changed:
+                                
+                                pipe.multi()
+                                #pipe.hset(self.lock_name, "writers", pickle.dumps(lock_writers))
+                                # since file will only be downloaded if key doesn't exist (because there's no way to check if local sequence
+                                # file is corrupted), and there's only one writer at a time, remove key so that redownload of file is attempted
+                                pipe.delete(self.lock_name)
+                                pipe.execute()
+                                
+                                print("Expired writers for key %s removed" % self.lock_name)
+                            
+                            time.sleep(5)
+                        
+                        else:
+                            
+                            print("No writers for key %s, adding reader" % self.lock_name)
+                            
+                            timestamp = time.time()
+                            
+                            lock_readers = pickle.loads(pipe.hget(self.lock_name, "readers"))
+                            
+                            lock_readers[self.uuid] = timestamp
+                            
+                            pipe.multi()
+                            pipe.hset(self.lock_name, "last-accessed", timestamp)
+                            pipe.hset(self.lock_name, "readers", pickle.dumps(lock_readers))
+                            pipe.execute()
+                            
+                            print("Reader for key %s acquired, returning file" % self.lock_name)
+                            
+                            break
+                    
+                except redis.exceptions.WatchError:
+                    pass
+            
+            
+            self.file = open(self.filepath, "r")
+            
+            return self.file
+        
+        def __exit__(self, exc_type, exc_value, traceback):
+            
+            if self.file is not None:
+                self.file.close()
+            
+            print("Releasing reader for key %s" % self.lock_name)
+            
+            pipe = self.conn.pipeline(True)
+            
+            while True:
+                
+                try:
+                    
+                    pipe.watch(self.lock_name)
+                    
+                    if pipe.exists(self.lock_name):
+                        
+                        lock_readers = pickle.loads(pipe.hget(self.lock_name, "readers"))
+                        
+                        if self.uuid in lock_readers:
+                            del lock_readers[self.uuid]
+                        
+                        pipe.multi()
                         pipe.hset(self.lock_name, "readers", pickle.dumps(lock_readers))
                         pipe.execute()
                         
-                        print("Reader for key %s acquired, returning file" % self.lock_name)
+                    else:
                         
-                        break
-                
-            except redis.exceptions.WatchError:
-                pass
-        
-        
-        self.file = open(self.filepath, "r")
-        
-        return self.file
+                        # read lock was broken by garbage collector
+                        
+                        pipe.unwatch()
+                    
+                    break
+                    
+                except redis.exceptions.WatchError:
+                    pass
+            
+            print("Reader for key %s released" % self.lock_name)
+            
+else:
     
-    def __exit__(self, exc_type, exc_value, traceback):
+    class CachedEntrezFile(object):
         
-        if self.file is not None:
-            self.file.close()
+        def __init__(self, seq_id):
+            
+            try:
+                Entrez.email = "6e6a62393840636f726e656c6c2e656475".decode("hex")
+                search_handle = Entrez.esearch(db="nucleotide", term=seq_id)
+                search_record = Entrez.read(search_handle)
+                search_handle.close()
+                self.seq_id = search_record["IdList"][0]
+            except IOError:
+                raise TaskError("Invalid sequence ID provided")
+            
+            self.file = None
         
-        print("Releasing reader for key %s" % self.lock_name)
-        
-        pipe = self.conn.pipeline(True)
-        
-        while True:
+        def __enter__(self):
             
             try:
                 
-                pipe.watch(self.lock_name)
+                fetch_handle = Entrez.efetch(db="nucleotide", id=self.seq_id, rettype="fasta", retmode="text")
+                fetch_record = SeqIO.read(fetch_handle, "fasta")
+                fetch_handle.close()
                 
-                if pipe.exists(self.lock_name):
-                    
-                    lock_readers = pickle.loads(pipe.hget(self.lock_name, "readers"))
-                    
-                    if self.uuid in lock_readers:
-                        del lock_readers[self.uuid]
-                    
-                    pipe.multi()
-                    pipe.hset(self.lock_name, "readers", pickle.dumps(lock_readers))
-                    pipe.execute()
-                    
-                else:
-                    
-                    # read lock was broken by garbage collector
-                    
-                    pipe.unwatch()
+                self.file = tempfile.TemporaryFile()
                 
-                break
+                SeqIO.write([fetch_record], self.file, "fasta")
                 
-            except redis.exceptions.WatchError:
-                pass
+                self.file.seek(0)
+                
+            except IOError:
+                raise TaskError("Could not download sequence from NCBI")
+            
+            return self.file
         
-        print("Reader for key %s released" % self.lock_name)
+        def __exit__(self, exc_type, exc_value, traceback):
+            
+            if self.file is not None:
+                self.file.close()
+    
