7010c094ecae78f1dda8fb6bf3447b2479158c9d
Nick Booher <njbooher@gmail.com>
2013-12-15 19:58:35 -0500

    Update off-target counting size limit error message to be more clear

--- findTAL.py
+++ findTAL.py
@@ -194,7 +194,7 @@ def validateOptions(options):
                 gene_length_total += len(gene.seq)
             
             if gene_length_total > 1000:
-                raise TaskError("Off-target counting is only available for inputs of 1000 bases or less")
+                raise TaskError("Off-target counting is only available when designing TALENs for sequences that are 1000 bases or less")
 
 def RunFindTALTask(options):
     
